2dd178026ffde02136d0793b74f04f83a931ade3
RF-Rifat <rfrifat6344@gmail.com>
2024-03-01 15:01:48 +0600

    checking new update

--- apps/ClintFile/src/component/module/CreateTodo.jsx
+++ apps/ClintFile/src/component/module/CreateTodo.jsx
@@ -1,7 +1,20 @@
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import style from './CreateTodo.module.css';
 
 const CreateTodo = (props) => {
+    const [checklistItems, setChecklistItems] = useState([]);
+
+    const addChecklistItem = () => {
+        setChecklistItems([...checklistItems, '']);
+    };
+
+    const handleChecklistChange = (index, value) => {
+        const updatedChecklistItems = [...checklistItems];
+        updatedChecklistItems[index] = value;
+        setChecklistItems(updatedChecklistItems);
+    };
+
     return (
         <div>
             <Modal
@@ -16,48 +29,58 @@ const CreateTodo = (props) => {
                             <div className={style.taskSecUp}>
                                 <div className={style.taskInputSec}>
                                     <label>Title <span className={style.redDot}>*</span></label>
-                                    <input type='text' placeholder='Enter Task Title' />
+                                    <input className={style.input} type='text' placeholder='Enter Task Title' />
                                 </div>
                                 <div className={style.selectPriority}>
                                     <div className={style.taskTitle}>
                                         <span>Select Priority <span className={style.redDot}>*</span></span>
                                     </div>
                                     <div className={style.selectOpt}>
-    <div className={`${style.opt} ${style.false}`}>
-        <span className={style.red}></span>
-        <span className={style.selectText}>HIGH PRIORITY</span>
-    </div>
-    <div className={`${style.opt} ${style.false}`}>
-        <span className={style.blue}></span>
-        <span className={style.selectText}>MODERATE PRIORITY</span>
-    </div>
-    <div className={`${style.opt} ${style.false}`}>
-        <span className={style.green}></span>
-        <span className={style.selectText}>LOW PRIORITY</span>
-    </div>
-</div>
-
+                                        <div className={`${style.opt} ${style.false}`}>
+                                            <span className={style.red}></span>
+                                            <span className={style.selectText}>HIGH PRIORITY</span>
+                                        </div>
+                                        <div className={`${style.opt} ${style.false}`}>
+                                            <span className={style.blue}></span>
+                                            <span className={style.selectText}>MODERATE PRIORITY</span>
+                                        </div>
+                                        <div className={`${style.opt} ${style.false}`}>
+                                            <span className={style.green}></span>
+                                            <span className={style.selectText}>LOW PRIORITY</span>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div>
                                     <div>
-                                        <span className={style.checklistTitle}>Checklist (0/0) <span className={style.redDot}>*</span></span>
+                                        <span className={style.checklistTitle}>Checklist ({checklistItems.length}/0) <span className={style.redDot}>*</span></span>
                                     </div>
-                                    <div className={style.taskInputs}></div>
-                                    <div className={style.taskAddBox}>
-                                        <span className={style.taskAddIcon}></span>
+                                    <div className={style.taskInputs}>
+                                        {checklistItems.map((item, index) => (
+                                            <input
+                                                key={index}
+                                                className={style.input}
+                                                type='text'
+                                                placeholder={`Checklist Item ${index + 1}`}
+                                                value={item}
+                                                onChange={(e) => handleChecklistChange(index, e.target.value)}
+                                            />
+                                        ))}
+                                    </div>
+                                    <div className={style.taskAddBox} onClick={addChecklistItem}>
+                                        <span className={style.taskAddIcon}>+</span>
                                         <span className={style.taskAddText}>Add New</span>
                                     </div>
                                 </div>
                             </div>
-                            <div className={style.taskSecDown}>
-                                <div>
-                                    <input className={style.dateBtn} type='date' />
-                                </div>
-                                <div className={style.buttonsRight}>
-                                    <button className={style.cancelBtn} onClick={props.onRequestClose}>Cancel</button>
-                                    <button disabled className={`${style.saveBtn} ${style.disableBtn}`}>Save</button>
-                                </div>
-                            </div>
+                        </div>
+                    </section>
+                    <section className={style.taskSecDown}>
+                        <div>
+                            <input className={style.dateBtn} type='date' />
+                        </div>
+                        <div className={style.buttonsRight}>
+                            <button className={style.cancelBtn} onClick={props.onRequestClose}>Cancel</button>
+                            <button disabled className={`${style.saveBtn} ${style.disableBtn}`}>Save</button>
                         </div>
                     </section>
                 </div>
